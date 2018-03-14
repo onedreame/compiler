8855ca9e4688216a5c1e38a8d942a66ebc12e1f3
onedreame <2980105328@qq.com>
2018-03-15 00:01:15 +0800

    debug parser

---
+++ ycc/include/code_gen.h
@@ -0,0 +1,8 @@
+//
+// Created by yk on 18-3-10.
+//
+
+#ifndef YCC_CODE_GEN_H
+#define YCC_CODE_GEN_H
+
+#endif //YCC_CODE_GEN_H
---
+++ ycc/src/code_gen.cpp
@@ -0,0 +1,5 @@
+//
+// Created by yk on 18-3-10.
+//
+
+#include "../include/code_gen.h"
